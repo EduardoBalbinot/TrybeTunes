6021a30182c109f81172068d52c652a71c804d76
Eduardo de Lima Balbinot <914ThePro@gmail.com>
2022-10-10 21:53:00 -0300

    requisito 5 feito

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,11 +2,39 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  state = {
+    artistSearch: '',
+  };
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
   render() {
+    const { artistSearch } = this.state;
+    const minSearchLenght = 2;
     return (
       <div data-testid="page-search">
         <Header />
         <h1>Search</h1>
+        <form>
+          <input
+            data-testid="search-artist-input"
+            placeholder="Nome do Artista"
+            name="artistSearch"
+            onChange={ this.handleChange }
+          />
+          <button
+            type="button"
+            data-testid="search-artist-button"
+            disabled={ artistSearch.length < minSearchLenght }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
