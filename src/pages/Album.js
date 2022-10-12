9140655fc226e68a9f50919fa71f288fda50e0a6
Eduardo de Lima Balbinot <914ThePro@gmail.com>
2022-10-12 00:13:14 -0300

    requisito 7 feito

--- src/pages/Album.js
+++ src/pages/Album.js
@@ -1,18 +1,47 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Header from '../components/Header';
+import getMusics from '../services/musicsAPI';
+import MusicCard from '../components/MusicCard';
 
 class Album extends React.Component {
-  render() {
+  state = {
+    album: [{
+      artistName: '',
+      collectionName: '',
+    }],
+  };
+
+  async componentDidMount() {
     const { match } = this.props;
     const { id } = match.params;
+    const album = await getMusics(id);
+    this.setState({
+      album,
+    });
+  }
+
+  render() {
+    const { album } = this.state;
+    const { artistName, collectionName } = album[0];
     return (
       <div data-testid="page-album">
         <Header />
-        <h1>
-          Album
-          { ` ${id}` }
-        </h1>
+        <h1 data-testid="artist-name">{ artistName }</h1>
+        <h2 data-testid="album-name">{ collectionName }</h2>
+        {
+          album.map((track, i) => (
+            i > 0
+              ? (
+                <MusicCard
+                  key={ track.trackId }
+                  trackName={ track.trackName }
+                  previewUrl={ track.previewUrl }
+                />
+              )
+              : ''
+          ))
+        }
       </div>
     );
   }
