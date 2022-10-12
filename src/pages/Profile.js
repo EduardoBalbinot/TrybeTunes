3d730eb513612f691ed55fca2431a260e98c2644
Eduardo de Lima Balbinot <914ThePro@gmail.com>
2022-10-12 17:50:05 -0300

    requisito 12 feito

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,12 +1,41 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
+import { getUser } from '../services/userAPI';
 
 class Profile extends React.Component {
+  state = {
+    user: {},
+    loading: false,
+  };
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const user = await getUser();
+    console.log(user);
+    this.setState({ user, loading: false });
+  }
+
   render() {
+    const { loading, user } = this.state;
     return (
       <div data-testid="page-profile">
         <Header />
         <h1>Profile</h1>
+        {
+          loading
+            ? (<Loading />)
+            : (
+              <div>
+                <img src={ user.image } alt={ user.name } data-testid="profile-image" />
+                <p>{ user.name }</p>
+                <p>{ user.email }</p>
+                <p>{ user.description }</p>
+                <Link to="profile/edit">Editar</Link>
+              </div>
+            )
+        }
       </div>
     );
   }
